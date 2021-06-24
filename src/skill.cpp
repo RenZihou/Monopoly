ddb6110a5c19b9a121b066d64ebb0e5aaf84ec96
RenZihou <renzihou.bj@gmail.com>
2021-06-24 11:44:16 +0800

    06/24 - skill (unfinished)

--- src/skill.cpp
+++ src/skill.cpp
@@ -3,3 +3,21 @@
 // @Author: RZH
 
 #include "skill.h"
+
+Skill::Skill(std::string name, std::string description,
+             std::vector<std::string> effect, std::vector<int> cool_down,
+             std::vector<std::string> condition) {
+    this->name = std::move(name);
+    this->description = std::move(description);
+    this->effect = std::move(effect);
+    this->cool_down = std::move(cool_down);
+    this->condition = std::move(condition);
+}
+
+std::string Skill::get_name() const {
+    return this->name;
+}
+
+std::string Skill::get_des() const {
+    return this->description;
+}
--- src/skill.h
+++ src/skill.h
@@ -1,15 +1,31 @@
 // skill.h
 // -*- encoding: utf-8 -*-
+// @Author: RZH
 
 #ifndef SKILL_H_
 #define SKILL_H_
 
 #include <string>
+#include <vector>
 
 class Skill {
 private:
     std::string name;
-    std::string effect;
+    std::string description;
+    std::vector<std::string> effect;
+    std::vector<int> cool_down;
+    std::vector<std::string> condition;
+
+public:
+    Skill(std::string name, std::string description,
+          std::vector<std::string> effect, std::vector<int> cool_down,
+          std::vector<std::string> condition);
+
+    ~Skill() = default;
+
+    std::string get_name() const;
+
+    std::string get_des() const;
 };
 
 #endif
